2e9f52a4e38268b898fe9aeef2b9482e74f71100
Fernando <FJCarranza@hotmail.com>
2021-02-21 10:35:01 -0300

    terminando Seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador.componet';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
-
+// Tarea:
+// Crear un modulo llamado contadorModule
+// Declaraciones y exportaciones 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contador/contador/contador.componet.ts
+++ src/app/contador/contador/contador.componet.ts
@@ -4,7 +4,10 @@ import {Component} from '@angular/core';
     selector: 'app-contador',
     template: `
         <h1>{{title}}</h1>
-        <h3><strong>La Base es: {{base}}</strong></h3>
+        <h3>
+            <strong>La Base es: {{base}}</strong>
+        </h3>
+        
         <button (click)="acumular(base)">+5</button>
         <span>{{numero}}</span>
         <button (click)="acumular(-base);">-5</button>
